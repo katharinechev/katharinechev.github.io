86a8df733589eda3c922d3136acb730cc797e3cf
katharinechev <113962819+katharinechev@users.noreply.github.com>
2022-09-20 11:57:51 -0600

    Add interactive scene project

---
+++ interactive-scene/sketch.js
@@ -0,0 +1,25 @@
+// Atari Breakout game - Interactive scene asssignment
+// Katharine C
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(220);
+}
+
+function draw() {
+
+}
+
+// blocks to get rid of
+function drawRectangles() { 
+  rect
+}
+
+function movingRectangle() {
+  
+}
