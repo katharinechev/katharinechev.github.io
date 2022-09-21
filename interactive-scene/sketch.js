be7e821a723e3c0369e9e5272a7735a5760b2fb2
katharinechev <113962819+katharinechev@users.noreply.github.com>
2022-09-21 12:17:04 -0600

    Added top rectangles
    Not fully formatted correctly

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,20 +6,33 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let cellWidth;
+let cellHeight;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(220);
 }
 
 function draw() {
-
+  drawRectangles();
 }
 
+// window resizing
+function windowResized() {
+  setup();
+}
 // blocks to get rid of
-function drawRectangles() { 
-  rect(x, y, );
+function drawRectangles() {
+  let cellWidth = width/14; //random(100, width/12);
+  let cellHeight = 50;
+  for (let y = 0; y < 3; y++) {
+    for (let x = 0; x < 14; x++) {
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+    }
+  }
+
+
 }
 
-function movingRectangle() {
-  
-}
+// function movingRectangle() {}
