aec7cddbdb5c83d31dcd989c0eaa34ba9fb533a9
katharinechev <113962819+katharinechev@users.noreply.github.com>
2022-09-22 12:17:12 -0600

    formatted top rectangles
    started sanity check for window resize

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -24,15 +24,23 @@ function windowResized() {
 }
 // blocks to get rid of
 function drawRectangles() {
-  let cellWidth = width/14; //random(100, width/12);
-  let cellHeight = 50;
-  for (let y = 0; y < 3; y++) {
+  let cellWidth = width/14; 
+  let cellHeight = height/20;
+
+  // if (cellHeight > cellWidth) {
+  //   cellWidth = cellHeight;
+  // }
+  // else {
+  //   cellHeight = cellWidth;
+  // }
+
+  for (let y = 0; y < 4; y++) {
     for (let x = 0; x < 14; x++) {
-      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+      rect(x*cellWidth, y*cellHeight +175, cellWidth, cellHeight);
     }
   }
-
-
 }
 
 // function movingRectangle() {}
+
+// function ball()
