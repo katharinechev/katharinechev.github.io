efa4605b6dec25fc9cb342e199df0efa59c9ae96
katharinechev <113962819+katharinechev@users.noreply.github.com>
2022-10-12 11:40:15 -0600

    finished collision demo for collide2D

--- collision-demo/sketch.js
+++ collision-demo/sketch.js
@@ -6,10 +6,35 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let hit = false;
+let colour;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  rect(200, 200, 100, 150);
+  circle(mouseX, mouseY, 100);
+
+  hit = collideRectCircle(200, 200, 100, 150, mouseX, mouseY, 100);
+
+  // Use vectors as input:
+  // const mouse      = createVector(mouseX, mouseY);
+  // const rect_start = createVector(200, 200);
+  // const rect_size  = createVector(100, 150);
+  // const radius     = 100;
+  // hit = collideRectCircleVector(rect_start, rect_size, mouse, radius);
+
+
+  if (hit) {
+    colour = "red";
+  }
+  else {
+    colour = "black";
+  }
+
+  stroke(colour);
+  print("colliding?", hit);
 }
