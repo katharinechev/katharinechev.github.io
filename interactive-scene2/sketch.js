e4ea92c341aa5deb1c4efec90a69420c3821cd61
katharinechev <113962819+katharinechev@users.noreply.github.com>
2022-10-15 20:05:17 -0600

    Added bounce off slider

--- interactive-scene2/sketch.js
+++ interactive-scene2/sketch.js
@@ -13,15 +13,15 @@
 let state = "start";
 let left = false;
 let right = false;
-let hit = false;
+let hitSlider = false;
 let dx = 6;
 let dy = 6;
 let score = 0;
-let circleRadius = 25;
+let circleDiameter = 25;
 let columns = 14;
 let rows = 8;
 let bricks = [];
-let movement, cellWidth, cellHeight, yRect, xButton, yButton, circleX, circleY;
+let movement, cellWidth, cellHeight, yRect, xButton, yButton, circleX, circleY, sliderHeight;
 let brickColour = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "white"];
 // let circleX = 100;
 // let circleY = 150;
@@ -35,7 +35,7 @@ let brickColour = ["red", "orange", "yellow", "green", "blue", "purple", "pink",
 function setup() {
   createCanvas(windowWidth, windowHeight);
   movement = width/2;
-  circleX = movement - 50;
+  circleX = movement + 50;
   circleY = height/2;
   createBricks(); 
   
@@ -52,7 +52,7 @@ function draw() {
     rectMode(CORNER);
     fill("white");
     drawBricks();
-    movingRectangle();
+    slider();
     drawBall();
     moveBall();
     ballPhysics();
@@ -62,6 +62,7 @@ function draw() {
 // window resizing
 function windowResized() {
   state = "start"
+  
   setup();
 }
 
@@ -96,16 +97,14 @@ function mouseInsideRect(left, right, top, bottom) {
 function createBricks() {
   cellWidth = width/columns; 
   cellHeight = height/30;
-  // fill(random(blockColour));
-  // fill("white")
 
   // window resizing approx consistancy
-  if (cellHeight >= cellWidth - 30) {
-    cellWidth = width/7;
-  }
-  else if (cellWidth <= cellHeight + 30) {
-    cellHeight = height/15;
-  }
+  // if (cellHeight >= cellWidth - 30) {
+  //   cellWidth = width/7;
+  // }
+  // else if (cellWidth <= cellHeight + 30) {
+  //   cellHeight = height/15;
+  // }
 
   for (let i = 0; i < columns; i++) {
     for (let j = 0; j < rows; j++) {
@@ -123,21 +122,21 @@ function createBricks() {
 }
 
 function drawBricks() {
-  bricks.forEach(brick => {
+  bricks.forEach(function(brick) {
     fill(brick.colour)
     rect(brick.x, brick.y, brick.w, brick.h)
   })
 }
 
-function movingRectangle() {
-  // let xRect = width/2; // not used atm
-  yRect = height - cellHeight +10;
+function slider() {
+  yRect = height - 1.5*cellHeight; 
+  sliderHeight = cellHeight-5;
 
   if (state === "game") {
     // if (mouseX > width-80) {
     //   mouseX = width-80;
     // }
-    rect(movement, yRect, 100, cellHeight - 10);
+    rect(movement, yRect, 100, sliderHeight);
     if (keyIsDown(65) && movement > 0) {
       movement -= 10;
     }
@@ -147,13 +146,9 @@ function movingRectangle() {
   }
 }
 
-
-
-
 function drawBall() {
-
   if (state === "game") {
-    ellipse(circleX, circleY, circleRadius, circleRadius);
+    ellipse(circleX, circleY, circleDiameter, circleDiameter);
   }
 }
 
@@ -167,25 +162,33 @@ function moveBall() {
 function ballPhysics() {
   if (state === "game") {
 
-    // ellipse(circleX, circleY, circleRadius, circleRadius);
-    if (circleX >= width - circleRadius) {
+    // ellipse(circleX, circleY, circleDiameter, circleDiameter);
+    if (circleX >= width - 0.5*circleDiameter) {
       dx *= -1;
     }
 
-    // if (circleY <= 0) {
-    //   dy = -dy;
+    if (circleY <= 0) {
+      dy = -dy;
     //   score++;
-    // }
+    }
     
-    // if (circleY >= height && circleX > width && circleX <= width/2) {
+    hitSlider = collideRectCircle(movement, yRect, 100, sliderHeight, circleX, circleY, circleDiameter);
+    if (hitSlider) {
+      dy *= -1;
+    }
+
+
+    // if (circleY >= height - 1.5*sliderHeight && circleX > movement && circleX <= movement +50) {
     //   dy = -dy;
 
     // }
 
-    // if (circleY >= height && circleX > width/2 && circleX <= width -170) {
+    // if (circleY >= height - 1.5*sliderHeight && circleX > movement && circleX <= movement +100) {
     //   dy = -dy;
       
     // }
+
+
 
   }
 }
