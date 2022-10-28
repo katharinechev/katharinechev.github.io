ff4a847d0808593e2b2df333e2f5b6ea037870b3
katharinechev <113962819+katharinechev@users.noreply.github.com>
2022-10-28 12:10:01 -0600

    added 2d array project

---
+++ 2d-array-project/sketch.js
@@ -0,0 +1,15 @@
+// 2D Array Project
+// Katharine
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
