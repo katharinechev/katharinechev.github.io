597435e45db7f6212ed0ecc523a3a65edeb3fe71
katharinechev <113962819+katharinechev@users.noreply.github.com>
2022-11-01 12:17:45 -0600

    made grid in centre

--- 2d-array-project/sketch.js
+++ 2d-array-project/sketch.js
@@ -11,18 +11,19 @@ let numSquares = ROWS*COLS;
 let cellWidth, cellHeight, grid;
 
 
-
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  cellHeight = width/2/ROWS;
-  cellWidth = height/2/COLS;
+  cellHeight = width/3/ROWS;
+  cellWidth = height/3/COLS;
+  if (cellHeight > cellWidth) {
+    cellWidth = cellHeight;
+  }
   grid = createRandom2dArray(COLS, ROWS);
 }
 
 function draw() {
   background(220);
-  displayGrid;
+  displayGrid(grid);
 }
 
 function createRandom2dArray(COLS, ROWS) {
@@ -30,12 +31,12 @@ function createRandom2dArray(COLS, ROWS) {
   for (let y = 0; y < ROWS; y++) {
     emptyArray.push([]);
     for (let x = 0; x < COLS; x++) {
-      let num = Math.floor(random(1, numSquares-2));
-      while (emptyArray.includes(num)) {
-        num = Math.floor(random(1, numSquares));
-      }
-      emptyArray[y].push(num);
-      
+      // let num = Math.floor(random(1, numSquares-2));
+      // if (emptyArray.includes(num) || emptyArray[y][x].includes(num)) {
+      //   num = Math.floor(random(1, numSquares));
+      // }
+      // emptyArray[y].push(num);
+      emptyArray[y].push(0);
     }
   }
   return emptyArray;
@@ -46,7 +47,7 @@ function displayGrid(grid) {
   for (let y = 0; y < ROWS; y++) {
     for (let x = 0; x < COLS; x++) {
       fill("white");
-      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
+      rect(x*cellWidth + (width/2-0.5*COLS*cellWidth), y*cellHeight + (height/5*3-0.5*COLS*cellWidth), cellWidth, cellHeight);
     }
   }
 }
